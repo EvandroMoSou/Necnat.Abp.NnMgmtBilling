60c6b6fcb2c3dd66c4cc7d8b75a847a2fef3b1aa
Evandro Mota de Souza <evandromosou@outlook.com>
2024-06-18 16:45:55 -0300

    att

--- src/Necnat.Abp.NnMgmtBilling.Domain.Shared/Domains/DmSku/SkuScopeConsts.cs
+++ src/Necnat.Abp.NnMgmtBilling.Domain.Shared/Domains/DmSku/SkuScopeConsts.cs
@@ -7,8 +7,8 @@
 
         public const string SkuIdDisplay = "SKU";
         public const string ApplicationNameDisplay = "Application Name";
-        public const string HttpMethodDisplay = "Http Method";
-        public const string UrlDisplay = "Url";
+        public const string HttpMethodDisplay = "HTTP Method";
+        public const string UrlDisplay = "URL";
         public const string IsBillableDisplay = "Billable";
         public const string IsActiveDisplay = "Active";
     }
